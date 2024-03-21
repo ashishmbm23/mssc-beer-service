4b7552574f67a3d585e59d76080fa918d2bb6bf8
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-03-21 14:49:32 +0530

    Implemented BOM.

--- src/main/java/com/ashish/msscbeerservice/bootstrap/DataLoader.java
+++ src/main/java/com/ashish/msscbeerservice/bootstrap/DataLoader.java
@@ -5,7 +5,6 @@ import com.ashish.msscbeerservice.repository.BeerRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.UUID;
