c6ffeab6512801f4d385fba5e0d1ed12a14b786a
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-04-09 15:24:40 +0530

    Change to DataLoader from mysql approach.

--- src/main/java/com/ashish/msscbeerservice/bootstrap/DataLoader.java
+++ src/main/java/com/ashish/msscbeerservice/bootstrap/DataLoader.java
@@ -5,13 +5,14 @@ import com.ashish.msscbeerservice.repository.BeerRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @AllArgsConstructor
 @Slf4j
-//@Service
+@Service
 public class DataLoader implements CommandLineRunner {
     private final BeerRepository beerRepository;
     @Override
@@ -33,24 +34,36 @@ public class DataLoader implements CommandLineRunner {
         UUID lagerUUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
         UUID aleUUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
         UUID porterUUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
-        Beer lager = Beer.builder().beerName("My Lager")
-                .beerStyle("LAGER")
+        Beer lager = Beer.builder().beerName("Mango Bobs")
+                .beerStyle("IPA")
                 .id(lagerUUID)
-                .upc("012345678901")
-                .minOnHand(100L)
-                .price(new BigDecimal(500))
+                .upc("0631234200036")
+                .quantityToBrew(100l)
+                .price(new BigDecimal(12.95))
+                .version(1L)
                 .build();
 
-        Beer ale = Beer.builder().beerName("My Ale")
+        Beer ale = Beer.builder().beerName("Galaxy Cat")
                 .id(aleUUID)
-                .beerStyle("ALE")
-                .upc("012345678911")
+                .beerStyle("PALE_ALE")
+                .upc("0631234300019")
                 .minOnHand(200L)
-                .price(new BigDecimal(1000))
+                .price(new BigDecimal(10.95))
+                .version(1L)
+                .build();
+
+        Beer porter = Beer.builder().beerName("Pinball Porter")
+                .id(porterUUID)
+                .beerStyle("PORTER")
+                .upc("0083783375213")
+                .minOnHand(300L)
+                .price(new BigDecimal(15.95))
+                .version(1L)
                 .build();
 
         beerRepository.save(lager);
         beerRepository.save(ale);
+        beerRepository.save(porter);
         log.info("exiting save beer");
     }
 }
