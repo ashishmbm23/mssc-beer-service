48f7c6b3fab54749681ecfba63e3d1c462f0f959
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-04-20 18:48:08 +0530

    Added configuration for JMS.

---
+++ src/main/java/com/ashish/msscbeerservice/config/JMSConfig.java
@@ -0,0 +1,21 @@
+package com.ashish.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfig {
+    public final String SFG_BEER_ORDER_QUEUE="sfg-beer-queue";
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTargetType(MessageType.TEXT);
+        messageConverter.setTypeIdPropertyName("_type");
+        return messageConverter;
+    }
+
+}
